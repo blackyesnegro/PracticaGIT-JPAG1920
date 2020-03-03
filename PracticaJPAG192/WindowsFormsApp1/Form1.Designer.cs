50a41e4f44452deb6baeade21e879dd5c709682a
Yeah Blacky <blackyesnegro@hotmail.com>
2020-03-03 14:15:40 +0100

    Cambio del textbox por NumberUpDown

--- PracticaJPAG192/WindowsFormsApp1/Form1.Designer.cs
+++ PracticaJPAG192/WindowsFormsApp1/Form1.Designer.cs
@@ -31,9 +31,10 @@
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.aluNombre = new System.Windows.Forms.TextBox();
-            this.aluNota = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.listaAlumnos = new System.Windows.Forms.TextBox();
+            this.aluNota = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.aluNota)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
@@ -62,13 +63,6 @@
             this.aluNombre.Size = new System.Drawing.Size(100, 22);
             this.aluNombre.TabIndex = 2;
             // 
-            // aluNota
-            // 
-            this.aluNota.Location = new System.Drawing.Point(359, 51);
-            this.aluNota.Name = "aluNota";
-            this.aluNota.Size = new System.Drawing.Size(100, 22);
-            this.aluNota.TabIndex = 3;
-            // 
             // label2
             // 
             this.label2.AutoSize = true;
@@ -87,19 +81,27 @@
             this.listaAlumnos.Size = new System.Drawing.Size(674, 276);
             this.listaAlumnos.TabIndex = 5;
             // 
+            // aluNota
+            // 
+            this.aluNota.Location = new System.Drawing.Point(359, 52);
+            this.aluNota.Name = "aluNota";
+            this.aluNota.Size = new System.Drawing.Size(120, 22);
+            this.aluNota.TabIndex = 6;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.aluNota);
             this.Controls.Add(this.listaAlumnos);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.aluNota);
             this.Controls.Add(this.aluNombre);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.aluNota)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -110,9 +112,9 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox aluNombre;
-        private System.Windows.Forms.TextBox aluNota;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox listaAlumnos;
+        private System.Windows.Forms.NumericUpDown aluNota;
     }
 }
 
